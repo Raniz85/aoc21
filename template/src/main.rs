faf830ba85d7c5beef34e4f81ae92642ee60edd5
Daniel Raniz Raneland <raniz@factor10.com>
2021-12-06 19:55:34 +0100

    Made a template I can copy for new days

---
+++ template/src/main.rs
@@ -0,0 +1,24 @@
+use anyhow::Result;
+use clap::Parser;
+use std::fs::File;
+use std::io::Read;
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+#[derive(Parser)]
+#[clap(version = "1.0", author = "Raniz")]
+struct Opts {
+    #[clap(short, long, default_value = "input")]
+    input: String,
+}
+
+fn main() -> Result<()> {
+    let opts: Opts = Opts::parse();
+    let mut input = String::new();
+    File::open(opts.input)?.read_to_string(&mut input)?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+}
